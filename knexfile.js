b24efe9a9df998e9c6e1dc0763370f06c5977809
alexandr.fufaev <alexander.fufaev@quantron-systems.com>
2021-05-10 13:29:44 +0300

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,23 +6,20 @@ const migrations = {
   directory: path.join(__dirname, 'server', 'migrations'),
 };
 
-const connectionStringExists = !!process.env.DATABASE_URL;
-const connectionOptions = connectionStringExists
-  ? { connectionString: process.env.DATABASE_URL }
-  : {
-    connection: {
+const isConnectionStringExists = !!process.env.DATABASE_URL;
+
+const settings = {
+  client: 'pg',
+  version: '13',
+  asyncStackTraces: true,
+  connection: isConnectionStringExists
+    ? process.env.DATABASE_URL
+    : {
       host: process.env.POSTGRES_HOST,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
     },
-  };
-
-const settings = {
-  client: 'pg',
-  version: '12',
-  asyncStackTraces: true,
-  ...connectionOptions,
   pool: {
     afterCreate(connection, done) {
       connection.query("SET timezone='UTC';", (error) => done(error, connection));
