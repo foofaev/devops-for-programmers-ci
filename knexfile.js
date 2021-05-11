7c76b226d877d4cbf86df48625e7305f5d0e4548
alexandr.fufaev <alexander.fufaev@quantron-systems.com>
2021-05-11 07:37:12 +0300

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,14 +6,17 @@ const migrations = {
   directory: path.join(__dirname, 'server', 'migrations'),
 };
 
-const isConnectionStringExists = !!process.env.DATABASE_URL;
+const isProduction = process.env.NODE_ENV === 'production';
 
 const settings = {
   client: 'pg',
   version: '13',
   asyncStackTraces: true,
-  connection: isConnectionStringExists
-    ? process.env.DATABASE_URL
+  connection: isProduction
+    ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    }
     : {
       host: process.env.POSTGRES_HOST,
       user: process.env.POSTGRES_USER,
